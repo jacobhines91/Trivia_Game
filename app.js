f265af7cf9e63ea60af8f89789721e59a5df49e2
Jacob Hines <jacob_hines@hotmail.com>
2019-08-24 22:17:07 -0500

    update 001--layout

---
+++ app.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+
+const start = document.getElementById("start-btn");
+
+const quiz = document.getElementById("question-screen");
+
+const choice1 = document.getElementById("option1");
+const choice2 = document.getElementById("option2");
+const choice3 = document.getElementById("option3");
+const choice4 = document.getElementById("option4");
+console.log(choice3);
+
+
+
+
+console.log(start)
+
+});
